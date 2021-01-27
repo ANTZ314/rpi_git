57b833ef31fb08803099e34ae50ab4cc8408bf3f
Antony Smith <antony.smith411@gmail.com>
2021-01-27 10:52:14 +0200

    gestled.py working

--- omnicode/test/gesture/gestled/ledClass.py
+++ omnicode/test/gesture/gestled/ledClass.py
@@ -10,8 +10,8 @@ import sys, time
 
 class LEDClass:
 	# Define Pins (Using GPIO not header numbers)
-	RED = LED(5)
-	GREEN = LED(6)
+	RED 	= LED(6)
+	GREEN 	= LED(5)
 	
 	def __init__(self, **kwargs):
 		print("LED Class Init!!")
@@ -23,45 +23,42 @@ class LEDClass:
 		
 		
 	def Blink_1(self):
-		RED.on()			# Green ON
+		self.RED.on()			# Green ON
 		time.sleep(0.2)
-		RED.off()			# Green OFF
+		self.RED.off()			# Green OFF
 		time.sleep(0.2)
-		RED.on()			# Green ON
+		self.RED.on()			# Green ON
 		time.sleep(0.2)
-		RED.off()			# Green OFF
+		self.RED.off()			# Green OFF
 		time.sleep(0.2)
-		print("Blink 1")
 		
 	def Blink_2(self):
-		try:
-			GREEN.on()			# Green ON
-			time.sleep(0.2)
-			GREEN.off()			# Green OFF
-			time.sleep(0.2)
-			GREEN.on()			# Green ON
-			time.sleep(0.2)
-			GREEN.off()			# Green OFF
-			time.sleep(0.2)
+		self.GREEN.on()			# Green ON
+		time.sleep(0.2)
+		self.GREEN.off()			# Green OFF
+		time.sleep(0.2)
+		self.GREEN.on()			# Green ON
+		time.sleep(0.2)
+		self.GREEN.off()			# Green OFF
+		time.sleep(0.2)
 		
-		print("Blink 2")
 
 	def Blink_3(self):
-		RED.on()			# Green ON
+		self.RED.on()			# Green ON
 		time.sleep(0.2)
-		RED.off()			# Green OFF
+		self.RED.off()			# Green OFF
 		time.sleep(0.2)
-		GREEN.on()			# Green ON
+		self.GREEN.on()			# Green ON
 		time.sleep(0.2)
-		GREEN.off()			# Green OFF
+		self.GREEN.off()			# Green OFF
 		time.sleep(0.2)
 
 	def Blink_4(self):
-		GREEN.on()			# Green ON
+		self.GREEN.on()			# Green ON
 		time.sleep(0.2)
-		GREEN.off()			# Green OFF
+		self.GREEN.off()			# Green OFF
 		time.sleep(0.2)
-		RED.on()			# Green ON
+		self.RED.on()			# Green ON
 		time.sleep(0.2)
-		RED.off()			# Green OFF
+		self.RED.off()			# Green OFF
 		time.sleep(0.2)
